9ab8c0313ee4d2f3bdcc0ec1b7587c95d540b396
Aizaz Rafique <aizaz58@yahoo.com>
2023-01-03 10:29:49 +0500

    frontend origin addded in cors

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,12 +1,8 @@
 const allowedOrigins=[
-<<<<<<< HEAD
-   'https://exercisetrack.netlify.app',
-    'http://localhost:5173',
-=======
     'http://localhost:3500',
     'http://localhost:5173',
     'http://localhost:3100',
->>>>>>> 2c1619c1b0f6459515c0f6f8803f6c038a51d6b4
+    'https://exercisetrack.netlify.app'
 ]
 
 module.exports=allowedOrigins
